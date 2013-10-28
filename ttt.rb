c1bd9e5d0c0888fe51699acf924c72f8f6f9255c
Michael Prouty <michael.prouty@gmail.com>
2013-10-28 18:26:25 -0400

    Implement Board class as 3x3 array of arrays

--- ttt.rb
+++ ttt.rb
@@ -8,18 +8,24 @@ class Board
   end
 
   def to_s
-    surface.each do |row|
-      puts row
-    end
+    return_string = ""
+    surface.each { |row| return_string += (row.to_s + "\n") }
+    return_string
   end
 
 end
 
 # 
 class Game
+  def initialize(board, player1, player2)
+    @game_board = board
+    @player1 = player1
+    @player2 = player2
+  end
 end
 
 class Player 
+  
 end
 
 class Move
