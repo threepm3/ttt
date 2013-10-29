ce493d10e09b922dc71634e968c58b9aa297d9d4
Michael Prouty <michael.prouty@gmail.com>
2013-10-29 17:10:11 -0400

    Wrap game setup in GamePlay#start, roughly implement Game#row_winner

--- ttt.rb
+++ ttt.rb
@@ -22,43 +22,52 @@ end
 # 
 class Game
   attr_accessor :board, :player1_char, :player2_char
-  def initialize(board, player1_char, player2)
-    @game_board = board
+  def initialize(player1_char, player2)
+    @board = Board.new
     @player1_char = player1_char
     @player2 = player2
   end
 
   def won?
-    surface.each do |row|
-      if row == ( Array.new(3, player1_char.char) )
-      end
+    row_won? || column_won? || diagonal_won?
+  end
+
+  def row_winner
+    player1_row = Array.new( 3, player1_char )
+    player2_row = Array.new( 3, player2_char )
+    
+    board.surface.each do |row| 
+      return player1_char if row == player1_row 
+      return player2_char if row == player2_row
     end
+  end
+
+  # Alrogithm: start with [0][0], [1][0], [2][0]
+  def column_won?
+    (0..2).each
+  end
+
+  def diagonal_won?
   end
 
 end
 
 # Takes care of user interaction
 class GamePlay
-  def start()
+  def self.start()
+    puts "Type a character to represent Player 1."
+    player1_char = gets[0] 
+    player2_char = nil
+    while player2_char == nil do
+      puts "Type a DIFFERENT character to represent Player 2."
+      buffer = gets[0]
+      player2_char = buffer unless (buffer == player1_char) 
+    end
+    board = Board.new
+    puts board
+    game = Game.new(board, player1_char, player2_char)
+    puts game
   end 
 
-  def get_move(player)
-    #returns [x,y]
-  end
-
-
 end
-
-puts "Type a character to represent Player 1 ==> "
-player1_char = gets[0] 
-player2_char = nil
-while player2_char == nil do
-  puts "Type a DIFFERENT character to represent Player 2. (Can't be '#{player1_char}') ==>"
-  buffer = gets[0]
-  player2_char = buffer unless (buffer == player1_char) 
-end
-board = Board.new
-puts board
-game = Game.new(board, player1_char, player2_char)
-puts game
 
