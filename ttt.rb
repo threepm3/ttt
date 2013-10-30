58787402d7ba9a4651d36db79919f0d482fa868e
Michael Prouty <michael.prouty@gmail.com>
2013-10-30 17:28:42 -0400

    Remove Game class, store players on GamePlay

--- ttt.rb
+++ ttt.rb
@@ -16,54 +16,6 @@ class Board
     nil
   end
 
-  def elements_match?(array)    
-    array.uniq.length == 1
-  end
-
-  # creates an array of 3-element arrays based on each win condition
-  def win_scenarios()
-    win_scenarios = []
-
-    # add rows and columns 
-    (0..2).each do |i|
-      win_scenarios.push(row(i))
-      win_scenarios.push(column(i))
-    end
-
-    # add diagonals
-    win_scenarios.push(diagonal)
-    win_scenarios.push(reverse_diagonal)
-
-    # return
-    win_scenarios
-  end
-
-  def column(i)
-    column = []
-    surface.each { |row| column.push(row[i]) } 
-    column
-  end
-
-  def row(i)
-    surface[i]
-  end
-
-  def diagonal()
-    diagonal = []
-    (0..2).each do |i|
-      diagonal.push(surface[i][i])
-    end
-    diagonal
-  end
-
-  def reverse_diagonal()
-    reverse_diagonal = []
-    (0..2).each do |i|
-      reverse_diagonal.push(surface[i][-(i+1)])
-    end
-    reverse_diagonal
-  end
-
   def to_s
     return_string = ""
     surface.each { |row| return_string += (row.to_s + "\n") }
@@ -77,42 +29,95 @@ class Board
       }
     }
   end
+
+  private
+
+    def elements_match?(array)    
+      array.uniq.length == 1
+    end
+
+    # returns an array of 3-element arrays based on each win condition
+    def win_scenarios()
+      win_scenarios = []
+
+      # add rows and columns 
+      (0..2).each do |i|
+        win_scenarios.push(row(i))
+        win_scenarios.push(column(i))
+      end
+
+      # add diagonals
+      win_scenarios.push(diagonal)
+      win_scenarios.push(reverse_diagonal)
+
+      # return
+      win_scenarios
+    end
+
+    def column(i)
+      column = []
+      surface.each { |row| column.push(row[i]) } 
+      column
+    end
+
+    def row(i)
+      surface[i]
+    end
+
+    def diagonal()
+      diagonal = []
+      (0..2).each do |i|
+        diagonal.push(surface[i][i])
+      end
+      diagonal
+    end
+
+    def reverse_diagonal()
+      reverse_diagonal = []
+      (0..2).each do |i|
+        reverse_diagonal.push(surface[i][-(i+1)])
+      end
+      reverse_diagonal
+    end
+
 end  
 
-# 
-class Game
-  attr_accessor :board, :player1_char, :player2_char
-  def initialize(player1_char, player2)
-    @board = Board.new
-    @player1_char = player1_char
-    @player2 = player2
-  end
-
-  def to_s
-    @board.to_s
-  end
-
-end
-
 # Takes care of user interaction
-# All class methods, b/c we don't want multiple simultaneous games
 class GamePlay
-  def self.start()
-    self.setup()
-    while 
-  end 
 
-  def self.setup()
-    puts "Player 1, select a character."
-    player1_char = gets[0] 
-    player2_char = nil
-    while player2_char == nil do
-      puts "Player 2, select a DIFFERENT character."
-      buffer = gets[0]
-      player2_char = buffer unless (buffer == player1_char) 
-    end
-    @@game = Game.new(player1_char, player2_char)
+  def start()
+    setup()
+    while @board.winner.nil? do
+      puts @board
+      get_move(@current_player)
+      
   end
 
-end
+  private
 
+    def setup()
+      puts "Player 1, select a character."
+      player1 = gets[0] 
+      player2 = nil
+      while player2 == nil do
+        puts "Player 2, select a DIFFERENT character."
+        buffer = gets[0]
+        player2 = buffer unless (buffer == player1) 
+      end
+      @board = Board.new
+      @players = [player1, player2]
+      set_random_turn
+    end
+
+    def set_random_turn()
+      @turn_index = [1, -1].sample
+    end
+
+    def end_turn
+      @turn_index *= -1
+    end
+
+    def get_move(player)
+      puts "Move for #{player}"
+    end
+end
